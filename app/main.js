c76c6e4f1e6cd59725b44bfefc026c60b24179b0
Aalok Tanwar <aaloktanwar88@gmail.com>
2024-04-05 10:10:56 +0530

    2nd stage

--- app/main.js
+++ app/main.js
@@ -9,6 +9,11 @@ const server = net.createServer((socket) => {
     socket.end();
     server.close();
   });
+
+  socket.on("data", () => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    socket.end();
+  });
 });
 
 server.listen(4221, "localhost");
