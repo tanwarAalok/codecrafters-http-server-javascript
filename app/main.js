b7b5ff2d83c0088bda64b90bf6fcf9b22f574465
Aalok Tanwar <aaloktanwar88@gmail.com>
2024-04-05 11:40:42 +0530

    5th stage

--- app/main.js
+++ app/main.js
@@ -3,6 +3,25 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+const parseRequest = (requestData) => {
+    const request = requestData.toString().split("\r\n");
+
+    console.log("Data: ", requestData);
+    console.log("Request: ", request);
+
+    const [method, path, protocol] = request[0].split(" ");
+
+    const headers = {};
+    request.slice(1).forEach((header) => {
+        const [key, value] = header.split(' ');
+        if (key && value) {
+            headers[key] = value;
+        }
+    })
+
+    return {method, path, protocol, headers}
+}
+
 const OK_RESPONSE = "HTTP/1.1 200 OK\r\n\r\n";
 const ERROR_RESPONSE = "HTTP/1.1 404 Not found\r\n\r\n";
 
@@ -13,8 +32,8 @@ const server = net.createServer((socket) => {
   });
 
   socket.on("data", (data) => {
-      const sections = data.toString().split("\r\n");
-      const path = sections[0].split(" ")[1];
+      const request = parseRequest(data);
+      const { method, path, protocol, headers } = request;
 
       if (path === "/") {
           socket.write(OK_RESPONSE)
@@ -22,6 +41,10 @@ const server = net.createServer((socket) => {
       else if (path.startsWith('/echo')) {
           const randomString = path.substring(6);
           socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${randomString.length}\r\n\r\n${randomString}`)
+      }
+      else if (path.startsWith('/user-agent')) {
+          const agent = request.headers["User-Agent:"];
+          socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${agent.length}\r\n\r\n${agent}`)
       }
       else socket.write(ERROR_RESPONSE)
 
